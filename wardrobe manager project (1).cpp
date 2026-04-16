5deeeabd2528199d7f7bc8653624753b186d2bd0
Aurelie Magloire <165699838+aureliemagloire@users.noreply.github.com>
2026-04-16 18:20:28 -0400

    Main
    Add laundry features

--- wardrobe manager project (1).cpp
+++ wardrobe manager project (1).cpp
@@ -1,11 +1,11 @@
 // Aurelie Magloire & Micah Burnett
 
-// Deliverable 1: Design + Linked List
+// Deliverable 3: Stack and Queue
 
 /* 
-- Implement Garment object with required attributes.
-- Implement Section storing garments using your linked list.
-- Deliverables: Garment + Section code, demo showing add/list/remove.
+- Implement Stack and Queue (one must use your linked list).
+
+- Add laundry and outfit planning features.
 */
 
 #include <iostream>
@@ -14,53 +14,80 @@
 #include "GarmentList.h"
 #include "Section.h"
 #include "Wardrobe.h"
+#include "LaundryStack.h"
+
 using namespace std;
 
 int main() {
+
+    // menu based CLI
+    cout << "-- Wardrobe Manager --" << endl;
+    cout << " 1. Add Section. \n 2. Add Garment. \n 3. Mark Dirty. \n 4. Wash next. \n 5. Plan Outfit. \n 6. Wear Next. \n 7. Show All. \n 0. Exit. \n";
+
     GarmentList list;
+    LaundryStack Hamper;
 
     // create garments
-Garment g1;
-    g1.setId(1);
-    g1.setName("jacket");
-    g1.setType("top");
-    g1.setColor("blue");
-    g1.setIsClean(true);
+    Garment g1;
+        g1.setId(1);
+        g1.setName(" jacket ");
+        g1.setType("top");
+        g1.setColor("blue");
+        g1.setIsClean(true);
 
-Garment g2;
-    g2.setId(2);
-    g2.setName("leggings");
-    g2.setType("bottoms");
-    g2.setColor("black");
-    g2.setIsClean(false);
+    Garment g2;
+        g2.setId(2);
+        g2.setName(" leggings ");
+        g2.setType("bottoms");
+        g2.setColor("black");
+        g2.setIsClean(false);
 
-Garment g3;
-    g3.setId(3);
-    g3.setName("tank top");
-    g3.setType("top");
-    g3.setColor("pink");
-    g3.setIsClean(true);
+    Garment g3;
+        g3.setId(3);
+        g3.setName(" tank ");
+        g3.setType("top");
+        g3.setColor("pink");
+        g3.setIsClean(true);
 
-    //create nodes 
+        //create nodes 
 
-    // test push back
-    list.PushBack(g1);
-    list.PushBack(g2);
-    list.PushBack(g3);
+        // test push back
+        list.PushBack(g1);
+        list.PushBack(g2);
+        list.PushBack(g3);
 
-    // test size traverse and find
-    cout << "Size: " << list.size() << endl;
-    cout << "List: " << endl;
-    list.Traverse();
-    cout << endl;
-    cout << "Find ID 2: " << list.FindById(2)->getName() << endl;
-    cout << endl;
+        // test size traverse and find
+        cout << "List: " << list.size() << " " << endl;
+        list.Traverse();
+        cout << endl;
 
-    // test remove
-    cout << "Removing ID 3: " << (list.RemoveById(2)) << endl;
+        cout << "\nFind ID 2: " << list.FindById(2)->getName() << endl;
+        cout << endl;
 
-    cout << "List: " << list.size() << endl;
-    list.Traverse();
+        // test remove
+        cout << "Removing ID: " << (list.RemoveById(2)) << endl;
+        
+        cout << "List: " << list.size() << " " << endl;
+        list.Traverse();
+        cout << endl;
 
-    return 0;
-}
+
+        // test laundry stack
+        cout << "Putting clothes in the hamper" << endl;
+        Hamper.Push(1); 
+        Hamper.Push(2);
+        Hamper.Push(3);
+        
+        cout << "Number of clothes in the hamper: " << Hamper.size() << endl;
+        
+        Hamper.Pop();
+
+        cout << "ID of clothes being washed: " << Hamper.Pop() << endl;
+        
+        cout << "Garmenets: " << list.size() << " " << endl;
+        list.Traverse();
+        cout << endl;
+
+
+        return 0;
+    }
