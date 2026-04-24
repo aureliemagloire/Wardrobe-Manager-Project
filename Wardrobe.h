c0e05f06d259672cf39ded9acb2dbf9a7409903d
Aurelie Magloire <165699838+aureliemagloire@users.noreply.github.com>
2026-04-23 21:44:19 -0400

    Wardrobe class and CLI

--- Wardrobe.h
+++ Wardrobe.h
@@ -1,22 +1,97 @@
 #ifndef WARDROBE_H
 #define WARDROBE_H
-#include "Garment.h"
 #include "GarmentList.h"
 #include "Section.h"
+#include "LaundryStack.h"
+#include "OutfitQueue.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
+struct SectionNode {
+    Section data;
+    SectionNode* next;
+    SectionNode(){
+        data = d;
+        next = nullptr;
+    }
+};
+
 class Wardrobe {
     private:
-        string wardrobeName;
-        Section* sections;
+        SectionNode* head;
+        LaundryStack laundry;
+        OutfitQueue outfits;
+        int nextId;
+
     public:
-        // constructor
-        Wardrobe(string name = ""){
-            wardrobeName = name;
-            sections = nullptr;
+        Wardrobe(){
+            head = nullptr;
         }
+
+        ~Wardrobe(){
+            Clear();
+        }
+
+        void addSection(string name){
+            Section newSection(name);
+            SectionNode* newNode = new SectionNode(newSection);
+
+            if (!head){
+                head = newNode;
+            } else {
+                SectionNode* temp = head;
+                while (temp->next) temp = temp->next;
+                temp->next = newNode;
+            }
+            cout << "Section " << name << "added." << endl;
+        }
+
+        void addClothestoSection(string sectionName, string name, string type, string color){
+            SectionNode* temp = head;
+            while (temp != nullptr){
+                if (temp->data.getName() == sectionName){
+                    Garment g(nextId++, name, type, color, true);
+                    temp->data.add(g);
+                    cout << "Added " << color << " " << name << " Id: " << g.getId() << " to " << sectionName << endl;
+                    return;
+                }
+                temp = temp->next;
+            }
+            cout << "Section not found." << endl;
+        }
+
+        // laundry
+
+        void markDirty(int id){
+            laundry.Push(id);
+            cout << "Garment ID " << id << " is in the hamper." << endl;
+        }
+
+        void washNext(){
+            int washed = laundry.Pop();
+            if (washed != -1){
+                cout << "Garment ID: " << washed << " has been washed." << endl;
+            }
+        }
+
+        // outfit 
+
+        void showAll(){
+            if (!head){
+                cout << "Wardrobe is empty." << endl;
+                return;
+            }
+
+            SectionNode* temp = head;
+            while (temp != nullptr){
+                cout << temp->data.getName() << " " << endl;
+                temp = temp->next;
+            }
+            cout << "Amount of garments in the hamper: " << laundry.size() << endl;
+            cout << "Outfits planned: " << outfits.size() << endl;
+        }
+
 };
 
 #endif
--- wardrobe manager project (1).cpp
+++ wardrobe manager project (1).cpp
@@ -1,12 +1,6 @@
 // Aurelie Magloire & Micah Burnett
 
-// Deliverable 3: Stack and Queue
-
-/* 
-- Implement Stack and Queue (one must use your linked list).
-
-- Add laundry and outfit planning features.
-*/
+// Final Deliverable
 
 #include <iostream>
 #include <string>
@@ -20,89 +14,144 @@
 using namespace std;
 
 int main() {
+    Wardrobe myWardrobe;
+    int choice = -1;
 
-    // menu based CLI
-    cout << "-- Wardrobe Manager --" << endl;
-    cout << " 1. Add Section. \n 2. Add Garment. \n 3. Mark Dirty. \n 4. Wash next. \n 5. Plan Outfit. \n 6. Wear Next. \n 7. Show All. \n 0. Exit. \n";
+    // section demo
+    myWardrobe.addSection("Tops");
+    myWardrobe.addSection("Bottoms");
+    myWardrobe.addClothestoSection("Tops", "tank", "shirt", "pink");
+    
+    while (choice != 0){
+        cout << "-- Wardrobe Manager --" << endl;
+        cout << " 1. Add Section. \n 2. Add Garment. \n 3. Mark Dirty. \n 4. Wash next. \n 5. Plan Outfit. \n 6. Wear Next. \n 7. Show All. \n 0. Exit. \n";
 
-    GarmentList list;
-    LaundryStack Hamper;
-    OutfitQueue outfitQueue;
+        cout << "ENTER: " << endl;
+        cin >> choice;
 
-    // create garments
-    Garment g1;
-        g1.setId(1);
-        g1.setName(" jacket ");
-        g1.setType("top");
-        g1.setColor("blue");
-        g1.setIsClean(true);
+        if (choice == 1){
+            string sectionName;
+            cout << "Enter a new section name: ";
+            cin >> sectionName;
+            myWardrobe.addSection(sectionName);
 
-    Garment g2;
-        g2.setId(2);
-        g2.setName(" leggings ");
-        g2.setType("bottoms");
-        g2.setColor("black");
-        g2.setIsClean(false);
-
-    Garment g3;
-        g3.setId(3);
-        g3.setName(" tank ");
-        g3.setType("top");
-        g3.setColor("pink");
-        g3.setIsClean(true);
-
-        //create nodes 
-
-        // test push back
-        list.PushBack(g1);
-        list.PushBack(g2);
-        list.PushBack(g3);
-
-        // test size traverse and find
-        cout << "List: " << list.size() << " " << endl;
-        list.Traverse();
-        cout << endl;
-
-        cout << "\nFind ID 2: " << list.FindById(2)->getName() << endl;
-        cout << endl;
-
-        // test remove
-        cout << "Removing ID: " << (list.RemoveById(2)) << endl;
+        } else if (choice == 2){
+            string sectionName, name, type, color;
+            cout << "Which section? ";
+            cin >> sectionName;
+            cout << "Garment Name: ";
+            cin >> name;
+            cout << "Garment Type: ";
+            cin >> type;
+            cout << "Garment Color: ";
+            cin >> color;
+            myWardrobe.addClothestoSection(sectionName, name, type, color);
         
-        cout << "List: " << list.size() << " " << endl;
-        list.Traverse();
-        cout << endl;
+        } else if (choice == 3){
+            int id;
+            cout << "Enter the Garment ID of dirty clothes: ";
+            cin >> id;
+            myWardrobe.markDirty(id);
 
+        } else if (choice == 4){
+            myWardrobe.washNext();
+        } else if (choice == 5){
+            int id;
+            cout << "Enter the Garment Id of your outfit: ";
+            cin >> id;
+            myWardrobe.outfits(id);
 
-        // test laundry stack
-        cout << "Putting clothes in the hamper" << endl;
-        Hamper.Push(1); 
-        Hamper.Push(2);
-        Hamper.Push(3);
+        } else if (choice == 6){
+            myWardrobe.wearNext();
+        } else if (choice == 7){
+            myWardrobe.showAll();
+
+        } else if (choice == 0) {
+            cout << "Exiting Wardrobe." << endl;
+        } else {
+            cout << "Invalid." << endl;
+        }
+    }
+
+    // GarmentList list;
+    // LaundryStack Hamper;
+    // OutfitQueue outfitQueue;
+
+    // // create garments
+    // Garment g1;
+    //     g1.setId(1);
+    //     g1.setName(" jacket ");
+    //     g1.setType("top");
+    //     g1.setColor("blue");
+    //     g1.setIsClean(true);
+
+    // Garment g2;
+    //     g2.setId(2);
+    //     g2.setName(" leggings ");
+    //     g2.setType("bottoms");
+    //     g2.setColor("black");
+    //     g2.setIsClean(false);
+
+    // Garment g3;
+    //     g3.setId(3);
+    //     g3.setName(" tank ");
+    //     g3.setType("top");
+    //     g3.setColor("pink");
+    //     g3.setIsClean(true);
+
+    //     //create nodes 
+
+    //     // test push back
+    //     list.PushBack(g1);
+    //     list.PushBack(g2);
+    //     list.PushBack(g3);
+
+    //     // test size traverse and find
+    //     cout << "List: " << list.size() << " " << endl;
+    //     list.Traverse();
+    //     cout << endl;
+
+    //     cout << "\nFind ID 2: " << list.FindById(2)->getName() << endl;
+    //     cout << endl;
+
+    //     // test remove
+    //     cout << "Removing ID: " << (list.RemoveById(2)) << endl;
         
-        cout << "Number of clothes in the hamper: " << Hamper.size() << endl;
+    //     cout << "List: " << list.size() << " " << endl;
+    //     list.Traverse();
+    //     cout << endl;
+
+
+    //     // test laundry stack
+    //     cout << "Putting clothes in the hamper" << endl;
+    //     Hamper.Push(1); 
+    //     Hamper.Push(2);
+    //     Hamper.Push(3);
         
-        Hamper.Pop();
-
-        cout << "ID of clothes being washed: " << Hamper.Pop() << endl;
+    //     cout << "Number of clothes in the hamper: " << Hamper.size() << endl;
         
-        cout << "Garmenets: " << list.size() << " " << endl;
-        list.Traverse();
-        cout << endl;
-//tesing outfit queue 
-cout << "Outfit Queue Test";
-    outfitQueue.enqueue(g1);
-    outfitQueue.enqueue(g2);
-    outfitQueue.enqueue(g3);
+    //     Hamper.Pop();
 
-cout << "Outfit Queue: "<<endl;
-outfitQueue.displayQueue();
+    //     cout << "ID of clothes being washed: " << Hamper.Pop() << endl;
+        
+    //     cout << "Garmenets: " << list.size() << " " << endl;
+    //     list.Traverse();
+    //     cout << endl;
 
-cout << "next item: "; << outfitQueue.peek().getName() 
-outfitQueue.dequeue();
+    //     //tesing outfit queue 
+    //     cout << "Outfit Queue Test";
+    //         outfitQueue.enqueue(g1);
+    //         outfitQueue.enqueue(g2);
+    //         outfitQueue.enqueue(g3);
 
-cout << "After that one item:"<<endl;
-outfitQueue.displayQueue();
+    //     cout << "Outfit Queue: "<<endl;
+    //     outfitQueue.displayQueue();
+
+    //     cout << "next item: "; << outfitQueue.peek().getName() 
+    //     outfitQueue.dequeue();
+
+    //     cout << "After that one item:"<<endl;
+    //     outfitQueue.displayQueue();
 
 
         return 0;
